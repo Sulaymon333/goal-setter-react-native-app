e98cb004f6f220b488ffb7d41b63be18a08cef09
tjtechi <tjsulaymon@gmail.com>
2019-11-24 10:48:51 +0200

    goal input functionality implemented and goal list displayed

--- App.js
+++ App.js
@@ -1,19 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const App = props => {
+    const [goalTitle, setGoalTitle] = useState('');
+    const [goals, setGoals] = useState([]);
+
+    const goalInputHandler = enteredText => {
+        setGoalTitle(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        setGoals([goalTitle, ...goals]);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter a goal"
+                    onChangeText={goalInputHandler}
+                    value={goalTitle}
+                />
+                <Button title="ADD" onPress={addGoalHandler} />
+            </View>
+            <View style={styles.goalList}>
+                {goals.map(goal => (
+                    <View key={goal} style={styles.goalItem}>
+                        <Text>{goal}</Text>
+                    </View>
+                ))}
+            </View>
+        </View>
+    );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        padding: 5,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        width: '80%'
+    },
+    goalList: {
+        marginVertical: 10
+    },
+    goalItem: {
+        paddingVertical: 7,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        backgroundColor: '#ddd9dd',
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 15
+    }
 });
